e6c14cfda96b08d9c35baa73ae52ed428cc0d674
code-by-mian <codebymian@gmail.com.com>
2026-07-08 15:06:35 +0500

    target : add x1q :init

--- target/x1q/debloat.sh
+++ target/x1q/debloat.sh
@@ -1,7 +1,7 @@
 # Copyright (c) 2025 Salvo Giangreco
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-# Debloat list for Galaxy S20 fe (Snapdragon) (r8q)
+# Debloat list for Galaxy S20 (Snapdragon) (x1q)
 # - Add entries inside the specific partition containing that file (<PARTITION>_DEBLOAT+="")
 # - DO NOT add the partition name at the start of any entry (eg. "/system/dpolicy_system")
 # - DO NOT add a slash at the start of any entry (eg. "/dpolicy_system")
