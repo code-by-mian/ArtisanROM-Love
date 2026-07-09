6d87ddc00b15babbfcc5aa52b8d62945ed679236
Love-ROM <codedbymian@gmail.com>
2026-07-09 20:02:21 +0500

    Update customize.sh
    Signed-off-by: Love-ROM <codedbymian@gmail.com>

--- platform/sm8250/patches/vendor/customize.sh
+++ platform/sm8250/patches/vendor/customize.sh
@@ -94,18 +94,3 @@ DELETE_FROM_WORK_DIR "vendor" "bin/hw/android.hardware.configstore@1.1-service"
 DELETE_FROM_WORK_DIR "vendor" "etc/init/android.hardware.configstore@1.1-service.rc"
 DELETE_FROM_WORK_DIR "vendor" "etc/seccomp_policy/configstore@1.1.policy"
 LOG_STEP_OUT
-
-LOG_STEP_IN "- Adding FBE v2 support"
-sed -i '\|/dev/block/bootdevice/by-name/userdata|c\
-/dev/block/bootdevice/by-name/userdata                 /data                  f2fs    noatime,nosuid,nodev,discard,usrquota,grpquota,fsync_mode=nobarrier,reserve_root=32768,resgid=5678,inlinecrypt    latemount,wait,check,fileencryption=aes-256-xts:aes-256-cts:v2+inlinecrypt_optimized,keydirectory=/metadata/vold/metadata_encryption,sysfs_path=/sys/devices/platform/soc/1d84000.ufshc,quota,reservedsize=128M,checkpoint=fs' \
-"$WORK_DIR/vendor/etc/fstab.qcom"
-LOG_STEP_OUT
-
-LOG_STEP_IN "- Remove Samsung Encryption"
-sed -i -E \
-    's/^([^#].*?)fileencryption=[^,]*(.*)$/# &\n\1encryptable\2/' \
-    "$WORK_DIR/vendor/etc/fstab.qcom"
-sed -i -E \
-    's/^([^#].*?)forceencrypt=[^,]*(.*)$/# &\n\1encryptable\2/' \
-    "$WORK_DIR/vendor/etc/fstab.qcom"
-LOG_STEP_OUT
