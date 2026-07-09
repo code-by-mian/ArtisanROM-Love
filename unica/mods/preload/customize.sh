d142e37da1ffd9d390e1c6fac255929fa441cdf8
code-by-mian <codebymian@gmail.com.com>
2026-07-09 09:17:06 +0500

    disable mods

--- unica/mods/preload/customize.sh
+++ unica/mods/preload/customize.sh
@@ -1,24 +1,51 @@
-# KernelSU Next
-KERNELSU_MANAGER_APK="https://github.com/KernelSU-Next/KernelSU-Next/releases/download/v3.2.0/KernelSU_Next_v3.2.0_33129-release.apk"
-# https://github.com/tiann/KernelSU/issues/886
-APK_PATH="system/preload/KernelSU-Next/com.rifsxd.ksunext-mesa==/base.apk"
+# Add KernelSU-Next Manager APK only for ESSI target
+if [ "$TARGET_OS_SINGLE_SYSTEM_IMAGE" = "essi" ]; then
+    KERNELSU_MANAGER_APK="https://github.com/KernelSU-Next/KernelSU-Next/releases/download/v3.2.0/KernelSU_Next_v3.2.0_33129-release.apk"
+    APK_PATH="system/preload/KernelSU-Next/com.rifsxd.ksunext-mesa==/base.apk"
 
-LOG "- Adding KernelSU-Next.apk to preload apps"
-mkdir -p "$WORK_DIR/system/$(dirname "$APK_PATH")"
-DOWNLOAD_FILE "$KERNELSU_MANAGER_APK" "$WORK_DIR/system/$APK_PATH"
+    LOG "- Adding KernelSU-Next APK v3.2.0 to preload apps"
+    mkdir -p "$WORK_DIR/system/$(dirname "$APK_PATH")"
+    DOWNLOAD_FILE "$KERNELSU_MANAGER_APK" "$WORK_DIR/system/$APK_PATH"
 
-while IFS= read -r i; do
-    i="${i//$WORK_DIR\/system\//}"
+    while IFS= read -r i; do
+        i="${i//$WORK_DIR\/system\//}"
 
-    if [ -d "$WORK_DIR/system/$i" ]; then
-        SET_METADATA "system" "$i" 0 0 755 "u:object_r:system_file:s0"
-    else
-        SET_METADATA "system" "$i" 0 0 644 "u:object_r:system_file:s0"
-    fi
+        if [ -d "$WORK_DIR/system/$i" ]; then
+            SET_METADATA "system" "$i" 0 0 755 "u:object_r:system_file:s0"
+        else
+            SET_METADATA "system" "$i" 0 0 644 "u:object_r:system_file:s0"
+        fi
 
-    if [[ "$i" == *".apk" ]] && \
-            ! grep -q "$i" "$WORK_DIR/system/system/etc/vpl_apks_count_list.txt"; then
-        LOG "- Adding \"$i\" to /system/system/etc/vpl_apks_count_list.txt"
-        EVAL "echo \"$i\" >> \"$WORK_DIR/system/system/etc/vpl_apks_count_list.txt\""
-    fi
-done <<< "$(find "$WORK_DIR/system/system/preload")"
+        if [[ "$i" == *".apk" ]] && \
+                ! grep -q "$i" "$WORK_DIR/system/system/etc/vpl_apks_count_list.txt"; then
+            LOG "- Adding \"$i\" to /system/system/etc/vpl_apks_count_list.txt"
+            EVAL "echo \"$i\" >> \"$WORK_DIR/system/system/etc/vpl_apks_count_list.txt\""
+        fi
+    done <<< "$(find "$WORK_DIR/system/system/preload")"
+fi
+
+# Add KernelSU-Next Manager APK only for QSSI target
+if [ "$TARGET_OS_SINGLE_SYSTEM_IMAGE" = "qssi" ]; then
+    KERNELSU_MANAGER_APK="https://github.com/KernelSU-Next/KernelSU-Next/releases/download/v3.1.0/KernelSU_Next_v3.1.0_33024-release.apk"
+    APK_PATH="system/preload/KernelSU-Next/com.rifsxd.ksunext-mesa==/base.apk"
+
+    LOG "- Adding KernelSU-Next APK v3.1.0 to preload apps"
+    mkdir -p "$WORK_DIR/system/$(dirname "$APK_PATH")"
+    DOWNLOAD_FILE "$KERNELSU_MANAGER_APK" "$WORK_DIR/system/$APK_PATH"
+
+    while IFS= read -r i; do
+        i="${i//$WORK_DIR\/system\//}"
+
+        if [ -d "$WORK_DIR/system/$i" ]; then
+            SET_METADATA "system" "$i" 0 0 755 "u:object_r:system_file:s0"
+        else
+            SET_METADATA "system" "$i" 0 0 644 "u:object_r:system_file:s0"
+        fi
+
+        if [[ "$i" == *".apk" ]] && \
+                ! grep -q "$i" "$WORK_DIR/system/system/etc/vpl_apks_count_list.txt"; then
+            LOG "- Adding \"$i\" to /system/system/etc/vpl_apks_count_list.txt"
+            EVAL "echo \"$i\" >> \"$WORK_DIR/system/system/etc/vpl_apks_count_list.txt\""
+        fi
+    done <<< "$(find "$WORK_DIR/system/system/preload")"
+fi
